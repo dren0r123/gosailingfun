54216b71187aa1f57d29a322b3b914482e6ba172
dren0r123 <dren0risme@gmail.com>
2026-04-06 12:33:17 +0300

    UPDATE / errors messages

--- src/const/errors.ts
+++ src/const/errors.ts
@@ -3,8 +3,7 @@ export const ERROR_MESSAGES = {
   INVALID_CERTIFICATE: 'Сертификат не найден или недействителен',
   YCLIENTS_VALIDATION_TOO_MANY_REQUESTS: 'Слишком много запросов к сервису валидации',
   CORS_NOT_ALLOWED: 'Политика CORS для этого сайта не разрешает доступ с указанного источника (Origin).',
-  TOO_MANY_REQUESTS_CERT_GEN:
-    'Слишком много запросов на генерацию сертификата, пожалуйста, повторите попытку через 1 минуту',
+  TOO_MANY_REQUESTS_CERT_GEN: 'Слишком много запросов, пожалуйста, повторите попытку через 1 минуту',
   SOMETHING_WENT_WRONG: 'Что-то пошло не так',
   INTERNAL_SERVER_ERROR: 'Внутренняя ошибка сервера',
   TEMPLATE_NOT_FOUND: 'Указанный шаблон дизайна не найден',
